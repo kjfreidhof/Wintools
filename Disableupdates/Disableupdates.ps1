f50785142d36de7c6b516c859872b175c19e67c6
Kyle Freidhof <freidhofkyle@gmail.com>
2023-06-15 12:40:21 -0400

    Adding Disable Automatic Updates script to wintools repo

---
+++ Disableupdates/Disableupdates.ps1
@@ -0,0 +1,14 @@
+# Check if the script is running with administrative privileges
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "Please run this script as an administrator."
+    Exit
+}
+
+# Create the registry key for Windows Update settings
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Force | Out-Null
+
+# Set the value to disable automatic updates
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Name "AUOptions" -Value 1 -Force | Out-Null
+
+Write-Host "Automatic updates have been disabled. Please restart your computer for the changes to take effect."
+
