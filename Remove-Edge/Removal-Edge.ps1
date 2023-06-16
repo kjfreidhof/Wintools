6cc8108a40598f13af95f98ea8fecf84be0a3ee9
Kyle Freidhof <freidhofkyle@gmail.com>
2023-06-16 19:18:19 -0400

    Removed feature Ask to set execution policy

--- Remove-Edge/Removal-Edge.ps1
+++ Remove-Edge/Removal-Edge.ps1
@@ -13,9 +13,7 @@ $edge = "Microsoft.MicrosoftEdge.Stable_8wekyb3d8bbwe"
 # SEtting varriable to get package name 
 $package = Get-AppxPackage -Name $edge -AllUsers
 
-$reset = Set-ExecutionPolicy -ExecutionPolicy Restricted
 
-$Get = Get-ExecutionPolicy 
 
 if ($null -ne $package ) {
     # Uninstalls the Microssoft edge package if found  
@@ -23,28 +21,11 @@ if ($null -ne $package ) {
     Remove-AppPackage -Package $edge -AllUsers
     Write-Host "Microsoft edge has been uninstalled"
     
-    $r1 = Read-Host "Do you want to restrict execution Policy?"
-
-if ($r1 -eq "y") {
-        $reset
-        Write-Host "Excecution Policy set to $Get"
-
-
-
-    } else {
-        Write-Host "Execution Policy is $Get"
-    }
 
 } else { # if not it will ssay edge not found
-    Write-Host "Microsoft Edge package is not found"
-    $read = Read-Host "Do you want to Restrict excectution policy"
-    
-    if ($read -eq "Y"){
-        $reset
-        Write-Host "Execution Policy is set to $Get"
-    } else {
-        Write-Host "EXecution Policy is $Get"
 
-    }
+    Write-Host "Microsoft Edge package is not found"
+    
+
 }
 
