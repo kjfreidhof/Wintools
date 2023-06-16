74f520a77ae8f2c6f5b3df05e66b81df7faf5695
Kyle Freidhof <freidhofkyle@gmail.com>
2023-06-16 15:27:56 -0400

    Added Comments to git script

--- Git/setup-git.ps1
+++ Git/setup-git.ps1
@@ -1,9 +1,25 @@
+# Script = Setup-git 
+
+# Author = kjfreidhof
+
+# Created = June, 15 2023 
+
+# About = A script that configures git for you 
+
+
+# Enter your Name 
 $user = Read-Host "Enter your name"
+# Enter Email 
 $mail = Read-Host "Enter your Email"
 
+# will set the Name for git
 git config --global user.name $user
-git config --global user.email $mail
 
+# will set the email for git 
+git config --global user.email $mail
+# outputs that Git was successfull
 Write-Host "Git configuration has been set"
+# Shows the name
 Write-Host "Name: $user"
+# shows the email 
 Write-Host "Email: $mail"
