43b4aa8457cb52058966608090e7fde9aff4154b
Kyle Freidhof <freidhofkyle@gmail.com>
2023-06-16 15:36:08 -0400

    Added Comments to Remove Microsoft store script

--- Remove-store/Removal-Store.ps1
+++ Remove-store/Removal-Store.ps1
@@ -1,10 +1,21 @@
+# Script = Removal Store
+
+# Author = kjfreidhof 
+
+# Created June, 15 2023 
+
+# About = A script that will remove the microsoft store 
+
+# Will get the Microsoft store package
 $store = Get-AppxPackage -AllUsers *WindowsStore*
 
+# if the microsoft store package is found it will Remove it 
 if ($store.Count -gt 0) {
     Write-Host "Uninstalling Microsoft Store"
     $store | Remove-AppPackage
     Write-Host "Microsoft-Store has been Uninstalled"
 
+    # if its not found it wont remove it 
 } else {
     Write-Host "Microsoft Store not found"
 }
