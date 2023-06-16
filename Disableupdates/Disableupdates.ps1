8720a4c492a8fb022358e3518664aa67117e14b5
Kyle Freidhof <freidhofkyle@gmail.com>
2023-06-16 15:25:47 -0400

    Added Comments to Disable automatic updates script

--- Disableupdates/Disableupdates.ps1
+++ Disableupdates/Disableupdates.ps1
@@ -1,3 +1,12 @@
+# Script = Disable Updates 
+
+# Author = kjfreidhof 
+
+# Created = June, 15 2023 
+
+# About = a Powershell script that Disables automatic updates in powershell 
+
+
 # Check if the script is running with administrative privileges
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Write-Host "Please run this script as an administrator."
@@ -11,4 +20,3 @@ New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Force 
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Name "AUOptions" -Value 1 -Force | Out-Null
 
 Write-Host "Automatic updates have been disabled. Please restart your computer for the changes to take effect."
-
